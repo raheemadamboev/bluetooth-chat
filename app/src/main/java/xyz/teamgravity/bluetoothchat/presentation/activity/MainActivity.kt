95d63620e56704b904f08a1ffc1c5b44a7e91787
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-21 18:14:02 +0500

    Installed AndroidEntryPoint to MainActivity;

--- app/src/main/java/xyz/teamgravity/bluetoothchat/presentation/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/presentation/activity/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.bluetoothchat.presentation.navigation.Navigation
 import xyz.teamgravity.bluetoothchat.presentation.theme.BluetoothChatTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Navigation()
                 }
             }
         }
