e1b6597921fe477e6eff7cfcdbca9f41cc7371ac
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-21 17:56:47 +0500

    Added MainNavGraph;

---
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/presentation/navigation/MainNavGraph.kt
@@ -0,0 +1,8 @@
+package xyz.teamgravity.bluetoothchat.presentation.navigation
+
+import com.ramcosta.composedestinations.annotation.NavGraph
+
+@NavGraph(default = true)
+annotation class MainNavGraph(
+    val start: Boolean = false,
+)
