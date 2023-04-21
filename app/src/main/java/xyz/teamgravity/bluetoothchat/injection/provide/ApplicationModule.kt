0df6fced6146cb21bc6b0d4ffd273694afcbf771
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-21 18:12:18 +0500

    Added ApplicationModule;

---
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/injection/provide/ApplicationModule.kt
@@ -0,0 +1,39 @@
+package xyz.teamgravity.bluetoothchat.injection.provide
+
+import android.app.Application
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.location.LocationManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import xyz.teamgravity.bluetoothchat.data.controller.AndroidBluetoothController
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideBluetoothManager(application: Application): BluetoothManager = application.getSystemService(BluetoothManager::class.java)
+
+    @Provides
+    @Singleton
+    fun provideBluetoothAdapter(bluetoothManager: BluetoothManager): BluetoothAdapter? = bluetoothManager.adapter
+
+    @Provides
+    @Singleton
+    fun provideAndroidBluetoothController(
+        application: Application,
+        bluetoothAdapter: BluetoothAdapter?,
+    ): AndroidBluetoothController = AndroidBluetoothController(
+        context = application,
+        adapter = bluetoothAdapter
+    )
+
+    @Provides
+    @Singleton
+    fun provideLocationManager(application: Application): LocationManager = application.getSystemService(LocationManager::class.java)
+}
