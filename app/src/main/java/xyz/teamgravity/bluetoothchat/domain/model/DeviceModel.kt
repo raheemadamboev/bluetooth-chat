bd5269b742ddcb267cfafae63daafe24d66c75d9
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-21 18:08:31 +0500

    Added DeviceModel;

--- app/src/main/java/xyz/teamgravity/bluetoothchat/domain/model/DeviceModel.kt
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/domain/model/DeviceModel.kt
@@ -1,6 +1,10 @@
 package xyz.teamgravity.bluetoothchat.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class DeviceModel(
     val name: String?,
     val address: String,
-)
+) : Parcelable
