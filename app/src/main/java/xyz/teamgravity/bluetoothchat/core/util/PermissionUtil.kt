9dc502cb2017f328e193a3d00ec1797244776234
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-21 18:07:32 +0500

    Added utils for Permission;

---
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/core/util/PermissionUtil.kt
@@ -0,0 +1,27 @@
+package xyz.teamgravity.bluetoothchat.core.util
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+object PermissionUtil {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // API
+    ///////////////////////////////////////////////////////////////////////////
+
+    fun canBluetoothConnect(context: Context): Boolean {
+        if (BuildUtil.deviceAtLeastS()) {
+            return ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED
+        }
+        return true
+    }
+
+    fun canBluetoothScan(context: Context): Boolean {
+        if (BuildUtil.deviceAtLeastS()) {
+            return ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED
+        }
+        return true
+    }
+}
