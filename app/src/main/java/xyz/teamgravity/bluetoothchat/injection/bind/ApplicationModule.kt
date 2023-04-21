73ff85608e75250c553f45957f314e813d64f0fc
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-21 18:13:00 +0500

    Added ApplicationModule;

---
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/injection/bind/ApplicationModule.kt
@@ -0,0 +1,18 @@
+package xyz.teamgravity.bluetoothchat.injection.bind
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import xyz.teamgravity.bluetoothchat.data.controller.AndroidBluetoothController
+import xyz.teamgravity.bluetoothchat.domain.controller.BluetoothController
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class ApplicationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindBluetoothController(androidBluetoothController: AndroidBluetoothController): BluetoothController
+}
