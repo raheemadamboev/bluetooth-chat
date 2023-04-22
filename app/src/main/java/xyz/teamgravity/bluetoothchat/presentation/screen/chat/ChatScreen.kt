46142ff812ae2646400a53a5f5a48a61224c50d1
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-22 17:54:25 +0500

    Built ChatScreen;

---
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/presentation/screen/chat/ChatScreen.kt
@@ -0,0 +1,170 @@
+package xyz.teamgravity.bluetoothchat.presentation.screen.chat
+
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.flow.collectLatest
+import xyz.teamgravity.bluetoothchat.R
+import xyz.teamgravity.bluetoothchat.domain.model.DeviceModel
+import xyz.teamgravity.bluetoothchat.presentation.navigation.MainNavGraph
+import xyz.teamgravity.bluetoothchat.presentation.theme.OldRose
+import xyz.teamgravity.bluetoothchat.presentation.theme.Vanilla
+
+@MainNavGraph
+@Destination(navArgsDelegate = ChatScreenNavArgs::class)
+@Composable
+fun ChatScreen(
+    state: LazyListState = rememberLazyListState(),
+    navigator: DestinationsNavigator,
+    viewmodel: ChatViewModel = hiltViewModel(),
+) {
+
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = viewmodel.event) {
+        viewmodel.event.collectLatest { event ->
+            when (event) {
+                is ChatViewModel.ChatEvent.ShowError -> Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show()
+                ChatViewModel.ChatEvent.Finish -> navigator.popBackStack()
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = viewmodel.messages.size) {
+        if (viewmodel.messages.isNotEmpty()) state.animateScrollToItem(viewmodel.messages.size - 1)
+    }
+
+    BackHandler {
+        viewmodel.onDisconnect()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.messages),
+                modifier = Modifier.weight(1F)
+            )
+            IconButton(onClick = viewmodel::onDisconnect) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = stringResource(id = R.string.disconnect)
+                )
+            }
+        }
+        if (viewmodel.connecting) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Text(text = stringResource(id = R.string.loading))
+            }
+        } else {
+            LazyColumn(
+                state = state,
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1F)
+            ) {
+                items(
+                    items = viewmodel.messages,
+                ) { message ->
+                    Box(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .clip(
+                                    RoundedCornerShape(
+                                        topStart = if (message.local) 15.dp else 0.dp,
+                                        topEnd = 15.dp,
+                                        bottomStart = 15.dp,
+                                        bottomEnd = if (message.local) 0.dp else 15.dp
+                                    )
+                                )
+                                .background(if (message.local) OldRose else Vanilla)
+                                .padding(16.dp)
+                                .align(if (message.local) Alignment.CenterEnd else Alignment.CenterStart)
+                        ) {
+                            Text(
+                                text = message.sender,
+                                fontSize = 10.sp,
+                                color = Color.Black,
+                            )
+                            Text(
+                                text = message.message,
+                                color = Color.Black,
+                            )
+                        }
+                    }
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                TextField(
+                    value = viewmodel.message,
+                    onValueChange = viewmodel::onMessageChange,
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.message))
+                    },
+                    modifier = Modifier.weight(1F)
+                )
+                IconButton(onClick = viewmodel::onSendMessage) {
+                    Icon(
+                        imageVector = Icons.Default.Send,
+                        contentDescription = stringResource(id = R.string.send_message)
+                    )
+                }
+            }
+        }
+    }
+}
+
+data class ChatScreenNavArgs(
+    val device: DeviceModel?,
+)
---
+++ app/src/main/java/xyz/teamgravity/bluetoothchat/presentation/screen/chat/ChatViewModel.kt
@@ -0,0 +1,100 @@
+package xyz.teamgravity.bluetoothchat.presentation.screen.chat
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import xyz.teamgravity.bluetoothchat.domain.controller.BluetoothController
+import xyz.teamgravity.bluetoothchat.domain.model.MessageModel
+import xyz.teamgravity.bluetoothchat.presentation.screen.destinations.ChatScreenDestination
+import javax.inject.Inject
+
+@HiltViewModel
+class ChatViewModel @Inject constructor(
+    handle: SavedStateHandle,
+    private val controller: BluetoothController,
+) : ViewModel() {
+
+    private val args = ChatScreenDestination.argsFrom(handle)
+
+    private val _event = Channel<ChatEvent>()
+    val event: Flow<ChatEvent> = _event.receiveAsFlow()
+
+    var connecting: Boolean by mutableStateOf(true)
+        private set
+
+    var message: String by mutableStateOf("")
+        private set
+
+    val messages: SnapshotStateList<MessageModel> = mutableStateListOf()
+
+    init {
+        getMessages()
+    }
+
+    private fun getMessages() {
+        viewModelScope.launch {
+            val messageFlow = if (args.device == null) controller.startServer() else controller.connect(args.device)
+            messageFlow.collectLatest { result ->
+                when (result) {
+                    BluetoothController.ConnectionResult.Established -> {
+                        connecting = false
+                    }
+
+                    is BluetoothController.ConnectionResult.Transferred -> {
+                        messages.add(result.message)
+                    }
+
+                    is BluetoothController.ConnectionResult.Error -> {
+                        connecting = false
+                        _event.send(ChatEvent.ShowError(result.message))
+                        _event.send(ChatEvent.Finish)
+                    }
+                }
+            }
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // API
+    ///////////////////////////////////////////////////////////////////////////
+
+    fun onMessageChange(value: String) {
+        message = value
+    }
+
+    fun onDisconnect() {
+        viewModelScope.launch {
+            controller.close()
+        }
+    }
+
+    fun onSendMessage() {
+        viewModelScope.launch {
+            val model = controller.sendMessage(message)
+            if (model != null) {
+                message = ""
+                messages.add(model)
+            }
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // MISC
+    ///////////////////////////////////////////////////////////////////////////
+
+    sealed interface ChatEvent {
+        data class ShowError(val message: String) : ChatEvent
+        object Finish : ChatEvent
+    }
+}
